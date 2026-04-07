877fea49c8b378d414b99906a538e4bf6268518e
Rohan Koli <kolirohan143@gmail.com>
2026-04-07 19:37:54 +0530

    make cart quatity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -66,6 +66,12 @@ document.querySelectorAll(".js-add-to-cart-button").forEach((button) => {
         quantity: 1,
       });
     }
+
+    let cartQuantity=0;
+    cart.forEach((item)=>{
+      cartQuantity+=item.quantity;
+    })
+    document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
     console.log(cart);
   });
 });
